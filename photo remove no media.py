e0e964bef9af6297110a674254f9335fbd2af8ff
IbrahimCPS <71455498+IbrahimCPS@users.noreply.github.com>
2022-06-03 22:05:09 +0100

    Add files via upload

---
+++ photo remove no media.py
@@ -0,0 +1,16 @@
+try:
+    file = open("New Text Document.txt", "r")
+    new_file = open("New_file.txt", "w").write("")
+    Done_file = open("New_file.txt", "a")
+    
+    for line in file.readlines():
+        sp = line.split()
+        def check(a):
+            if a == "_":
+                return ""
+            else:
+                return "g"
+        Done_file.write("ren " + sp[0] + " " + sp[0][0:23] + check(sp[0][23]) + "\n" + "ren " + sp[1] + " " + sp[1][0:23] + check(sp[1][23]) + "\n" + "ren " + sp[2] + " " + sp[2][0:23] + check(sp[2][23]) + "\n")
+finally:
+    file.close()
+    Done_file.close()
